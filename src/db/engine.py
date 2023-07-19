ef55c7f6c13b58f72f61933e7670fe7a8787e855
Bastian Hoyer <dafire@gmail.com>
2023-07-19 18:57:05 +0200

    update

--- src/db/engine.py
+++ src/db/engine.py
@@ -1,8 +1,14 @@
+from orjson import orjson
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from settings import DatabaseSettings, get_settings
 
 _settings = get_settings(DatabaseSettings)
+
+
+def orjson_serializer(obj):
+    return orjson.dumps(obj, option=orjson.OPT_NAIVE_UTC).decode()
+
 
 async_engine = create_async_engine(
     _settings.url,
@@ -10,8 +16,7 @@ async_engine = create_async_engine(
     pool_pre_ping=True,
     pool_use_lifo=True,
     echo=_settings.echo,
+    json_serializer=orjson_serializer,
 )
-async_session_factory = async_sessionmaker(
-    bind=async_engine,
-    expire_on_commit=False,
-)
+
+async_session_factory = async_sessionmaker(bind=async_engine, expire_on_commit=False)
