849d28aa2065ebba63474c6f70d197e76f124d60
dafire <da.fire@gmail.com>
2023-08-25 17:41:31 +0200

    update

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -4,7 +4,6 @@ import * as Turbo from "@hotwired/turbo"
 import {Application} from "@hotwired/stimulus"
 import {definitionsFromContext} from "@hotwired/stimulus-webpack-helpers"
 
-
 let Stimulus = Application.start()
 const context = require.context("./controllers", true, /\.ts$/)
 Stimulus.load(definitionsFromContext(context))
